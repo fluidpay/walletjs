636df121e569e11cadc263ae5cdd75d1607e833f
Steve Kroll <steve.m.kroll@gmail.com>
2021-03-29 15:22:54 -0500

    formatting

--- src/wallet.js
+++ src/wallet.js
@@ -1,6 +1,6 @@
 import { applepay } from "./_applepay.js";
-// import creditcard from "./_creditcard.js";
-// import googlepay from "./_googlepay.js";
+// import { creditcard } from "./_creditcard.js";
+// import { googlepay } from "./_googlepay.js";
 
 export const walletjs = {
   applepay,
