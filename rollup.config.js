10a48f2800156dbf635b4171fc1c09bfd8c55dbe
Conner Hewitt <chewitt@fluidpay.com>
2021-08-09 16:12:38 -0500

    Fixed UMD export, made it default walletjs.js dist file

--- rollup.config.js
+++ rollup.config.js
@@ -7,21 +7,6 @@ export default [
     input: "src/walletjs.js",
     output: [
       {
-        file: "dist/walletjs.js",
-        format: "esm",
-        name: "walletjs",
-        exports: "named",
-        sourcemap: false
-      },
-      {
-        file: "dist/walletjs.umd.js",
-        format: "umd",
-        name: "walletjs",
-        esModule: false,
-        exports: "named",
-        sourcemap: false
-      },
-      {
         file: "dist/walletjs.esm.js",
         format: "esm",
         name: "walletjs",
@@ -32,6 +17,34 @@ export default [
         file: "dist/walletjs.cjs.js",
         format: "cjs",
         name: "walletjs",
+        exports: "named",
+        sourcemap: false
+      }
+    ],
+    plugins: [
+      resolve(),
+      babel({
+        babelHelpers: "bundled"
+      }),
+      terser()
+    ]
+  },
+  {
+    input: "src/walletjs-umd.js",
+    output: [
+      {
+        file: "dist/walletjs.js",
+        format: "umd",
+        name: "walletjs",
+        esModule: false,
+        exports: "named",
+        sourcemap: false
+      },
+      {
+        file: "dist/walletjs.umd.js",
+        format: "umd",
+        name: "walletjs",
+        esModule: false,
         exports: "named",
         sourcemap: false
       }
